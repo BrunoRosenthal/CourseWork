42ef2357ad33434e363ec7bc4006098e7d3d2f0d
BrunoRosenthal <72097131+BrunoRosenthal@users.noreply.github.com>
2020-10-31 12:07:19 +0000

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -8,12 +8,11 @@ def quiz(quiz, user):
     score = 0
     cursor.execute("SELECT * FROM questions WHERE quizID=?;", [(quiz)])
     questions = cursor.fetchall()
-    print(questions)
     numQu = 0
 
     for question in questions:
         topic = question[1]
-        print("What is the value of"  + question[2])
+        print("What is the value of "  + question[2])
         print("\n 1. %s \n 2. %s \n 3. %s \n 4. %s \n" %(question[3], question[4], question[5], question[6]))
         choice = input("Which of these is the answer? ")
         if choice == question[7]:
@@ -23,10 +22,11 @@ def quiz(quiz, user):
         else:
             print("Incorrect")
         numQu +=1
+        print("You have finished this quiz")
 
     scorePercent = int((score/numQu)*100)
-    print("You scored" + scorePercent +"%")
+    print("You scored %s percent" %scorePercent)
 
-    insertData = ("INSERT INTO scores(userID, score, topicID) VALUES(?,?,?);")
-    cursor.execute(insertData, [(user, (scorePercent), (quiz))])
+    insertData = ("INSERT INTO scores(userID, score, quizID) VALUES(?,?,?);")
+    cursor.execute(insertData, [(user), (scorePercent), (quiz)])
     db.commit()

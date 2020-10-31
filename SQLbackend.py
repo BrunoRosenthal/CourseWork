9047a8018c4a0d8ca1b3e44ab5d2116d9fa09457
BrunoRosenthal <72097131+BrunoRosenthal@users.noreply.github.com>
2020-10-31 11:53:26 +0000

    Update SQLbackend.py

--- SQLbackend.py
+++ SQLbackend.py
@@ -1,79 +1,47 @@
 import sqlite3
 
-# creating the database for the quiz if it doesn't already exist
-
 with sqlite3.connect("quiz.db")as db:
     cursor = db.cursor()
 
-# creating the table for users if it doesn't already exist
+cursor.execute("""DELETE FROM quizzes""")
+db.commit()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS user(
-userID INTEGER PRIMARY KEY,
-username VARCHAR(20) NOT NULL,
-Fname VARCHAR(20) NOT NULL,
-Lname VARCHAR(20) NOT NULL,
-password VARCHAR(20) NOT NULL);
+INSERT INTO quizzes(quizName)
+VALUES("Addition"),("Subtraction"),("Multiplication"),("Division"),("Advanced arithmetic");
 """)
+db.commit()
 
-# creating table for the individual quizzes
+cursor.execute("""DELETE FROM questions""")
+db.commit()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS quizzes(
-quizID INTEGER PRIMARY KEY,
-quizName VARCHAR(20) NOT NULL);
+INSERT INTO questions(quizID,question,option1,option2,option3,option4,answer)
+VALUES("1","2+2","4","5","6","7","1"),
+("1","2+3","4","5","6","7","2"),
+("1","3+4","4","5","6","7","4"),
+
+("2","2-1","1","5","6","7","1"),
+("2","3-2","1","5","6","7","1"),
+("2","3-1","2","5","6","7","1"),
+
+("3","2x1","2","5","6","7","1"),
+("3","3x2","1","3","6","7","3"),
+("3","3x1","2","3","6","7","2"),
+
+("3","2x1","2","5","6","7","1"),
+("3","3x2","1","3","6","7","3"),
+("3","3x1","2","3","6","7","2"),
+
+("4","2/1","2","5","6","7","1"),
+("4","6/2","1","3","6","7","2"),
+("4","3/1","2","3","6","7","2"),
+
+("5","2^1","2","5","6","7","1"),
+("5","6^2","1","3","6","36","4"),
+("5","9^(1/2)","2","3","6","7","1");
 """)
+db.commit()
 
-# creating the table for the scores
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS scores(
-scoreID INTEGER PRIMARY KEY,
-userID INTEGER NOT NULL,
-score INTEGER NOT NULL,
-quizID INTEGER NOT NULL,
-FOREIGN KEY(userID) REFERENCES user(userID),
-FOREIGN KEY(quizID) REFERENCES quizzes(quizID));
-""")
 
-# creating a table for the questions
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS questions(
-questionID INTEGER PRIMARY KEY,
-quizID INTEGER NOT NULL,
-question VARCHAR(100) NOT NULL,
-option1 VARCHAR(50) NOT NULL,
-option2 VARCHAR(50) NOT NULL,
-option3 VARCHAR(50),
-option4 VARCHAR(50),
-answer VARCHAR(50),
-FOREIGN KEY(quizID) REFERENCES quizzes(quizID));
-""")
-
-# TESTING CODE IS ALL BELOW:
-
-# to delete all records
-
-# cursor.execute("""DELETE FROM quizzes""")
-# db.commit()
-
-# to print all records
-
-# cursor.execute("SELECT * FROM user")
-# print(cursor.fetchall())
-
-# to create test users
-
-# cursor.execute(""" INSERT INTO user(username,Fname,Lname,password)
-# VALUES("John2003","John","Smith","guest")""")
-# db.commit()
-
-# to show all tables
-
-# tables = cursor.execute("""
-# SELECT name FROM sqlite_master
-# WHERE type="table"
-# ORDER BY name;
-# """)
-# print(cursor.fetchall())

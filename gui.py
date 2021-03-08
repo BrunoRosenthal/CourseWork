0c03e0f70b7acfee21472878714eb2e77c863998
BrunoRosenthal <72097131+BrunoRosenthal@users.noreply.github.com>
2021-03-08 22:27:44 +0000

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import sqlite3
+import time
 
 
 # noinspection PyAttributeOutsideInit
@@ -20,53 +21,53 @@ class Gui(tk.Frame):
     def startMenu(self):
         self.clearFrame()
 
-        self.createAccountBtn = tk.Button(self.master, text='Create account', command=lambda: self.createAccount())
-        self.createAccountBtn.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+        createAccountBtn = tk.Button(self.master, text='Create account', command=lambda: self.createAccount())
+        createAccountBtn.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-        self.loginBtn = tk.Button(self.master, text='Login', command=lambda: self.login())
-        self.loginBtn.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+        loginBtn = tk.Button(self.master, text='Login', command=lambda: self.login())
+        loginBtn.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
 
-        self.quitBtn = tk.Button(self.master, text="Quit", command=self.master.destroy)
-        self.quitBtn.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
+        quitBtn = tk.Button(self.master, text="Quit", command=self.master.destroy)
+        quitBtn.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
 
     def createAccount(self):
         self.clearFrame()
 
-        self.nameLabel = tk.Label(self.master, text='Name:')
-        self.nameLabel.grid(row=0, column=0, padx=10, pady=10)
+        nameLabel = tk.Label(self.master, text='Name:')
+        nameLabel.grid(row=0, column=0, padx=10, pady=10)
 
         self.nameBox = tk.Entry(self.master)
         self.nameBox.grid(row=0, column=1, padx=10, pady=10)
 
-        self.surnameLabel = tk.Label(self.master, text='Surame:')
-        self.surnameLabel.grid(row=1, column=0, padx=10, pady=10)
+        surnameLabel = tk.Label(self.master, text='Surame:')
+        surnameLabel.grid(row=1, column=0, padx=10, pady=10)
 
         self.surnameBox = tk.Entry(self.master)
         self.surnameBox.grid(row=1, column=1, padx=10, pady=10)
 
-        self.usernameLabel = tk.Label(self.master, text='Username:')
-        self.usernameLabel.grid(row=2, column=0, padx=10, pady=10)
+        usernameLabel = tk.Label(self.master, text='Username:')
+        usernameLabel.grid(row=2, column=0, padx=10, pady=10)
 
         self.usernameBox = tk.Entry(self.master)
         self.usernameBox.grid(row=2, column=1, padx=10, pady=10)
 
-        self.passwordLabel = tk.Label(self.master, text='Password:')
-        self.passwordLabel.grid(row=3, column=0, padx=10, pady=10)
+        passwordLabel = tk.Label(self.master, text='Password:')
+        passwordLabel.grid(row=3, column=0, padx=10, pady=10)
 
         self.passwordBox = tk.Entry(self.master)
         self.passwordBox.grid(row=3, column=1, padx=10, pady=10)
 
-        self.confirmPasswordLabel = tk.Label(self.master, text='Confirm password:')
-        self.confirmPasswordLabel.grid(row=4, column=0, padx=10, pady=10)
+        confirmPasswordLabel = tk.Label(self.master, text='Confirm password:')
+        confirmPasswordLabel.grid(row=4, column=0, padx=10, pady=10)
 
         self.confirmPasswordBox = tk.Entry(self.master)
         self.confirmPasswordBox.grid(row=4, column=1, padx=10, pady=10)
 
-        self.backBtn = tk.Button(self.master, text="Back", command=lambda: self.startMenu())
-        self.backBtn.grid(row=5, column=0, columnspan=1, padx=10, pady=10)
+        backBtn = tk.Button(self.master, text="Back", command=lambda: self.startMenu())
+        backBtn.grid(row=5, column=0, columnspan=1, padx=10, pady=10)
 
-        self.enterBtn = tk.Button(self.master, text="Enter", command=lambda: self.getCreateInputs())
-        self.enterBtn.grid(row=5, column=1, columnspan=1, padx=10, pady=10)
+        enterBtn = tk.Button(self.master, text="Enter", command=lambda: self.getCreateInputs())
+        enterBtn.grid(row=5, column=1, columnspan=1, padx=10, pady=10)
 
     def getCreateInputs(self):
         self.fname = self.nameBox.get()
@@ -109,43 +110,43 @@ class Gui(tk.Frame):
 
             self.clearFrame()
 
-            self.created = tk.Label(self.master, text='Your account has been successfully created')
-            self.created.grid(row=0, column=0)
+            created = tk.Label(self.master, text='Your account has been successfully created')
+            created.grid(row=0, column=0)
 
-            self.ok = tk.Button(self.master, text='ok', command=lambda: self.mainMenu())
-            self.ok.grid(row=1, column=0)
+            ok = tk.Button(self.master, text='ok', command=lambda: self.mainMenu())
+            ok.grid(row=1, column=0)
 
         else:
             problems = ', '.join(errors)
 
             self.clearFrame()
 
-            self.passwordLenError = tk.Label(self.master, text='Your account was not created because '+problems)
-            self.passwordLenError.grid(row=0, column=0)
+            passwordLenError = tk.Label(self.master, text='Your account was not created because '+problems)
+            passwordLenError.grid(row=0, column=0)
 
-            self.ok = tk.Button(self.master, text='ok', command=lambda: self.createAccount())
-            self.ok.grid(row=1, column=0)
+            ok = tk.Button(self.master, text='ok', command=lambda: self.createAccount())
+            ok.grid(row=1, column=0)
 
     def login(self):
         self.clearFrame()
 
-        self.usernameLabel = tk.Label(self.master, text='Username:')
-        self.usernameLabel.grid(row=0, column=0, padx=10, pady=10)
+        usernameLabel = tk.Label(self.master, text='Username:')
+        usernameLabel.grid(row=0, column=0, padx=10, pady=10)
 
         self.usernameBox = tk.Entry(self.master)
         self.usernameBox.grid(row=0, column=1, padx=10, pady=10)
 
-        self.passwordLabel = tk.Label(self.master, text='Password:')
-        self.passwordLabel.grid(row=1, column=0, padx=10, pady=10)
+        passwordLabel = tk.Label(self.master, text='Password:')
+        passwordLabel.grid(row=1, column=0, padx=10, pady=10)
 
         self.passwordBox = tk.Entry(self.master)
         self.passwordBox.grid(row=1, column=1, padx=10, pady=10)
 
-        self.backBtn = tk.Button(self.master, text="Back", command=lambda: self.startMenu())
-        self.backBtn.grid(row=2, column=0, columnspan=1, padx=10, pady=10)
+        backBtn = tk.Button(self.master, text="Back", command=lambda: self.startMenu())
+        backBtn.grid(row=2, column=0, columnspan=1, padx=10, pady=10)
 
-        self.enterBtn = tk.Button(self.master, text="Enter", command=lambda: self.getLoginInputs())
-        self.enterBtn.grid(row=2, column=1, columnspan=1, padx=10, pady=10)
+        enterBtn = tk.Button(self.master, text="Enter", command=lambda: self.getLoginInputs())
+        enterBtn.grid(row=2, column=1, columnspan=1, padx=10, pady=10)
 
     def getLoginInputs(self):
         self.username = self.usernameBox.get()
@@ -162,54 +163,53 @@ class Gui(tk.Frame):
         if valid_user:
             self.clearFrame()
 
-            self.welcomeMessage = tk.Label(self.master, text="Welcome "+self.username)
-            self.welcomeMessage.grid(row=0, column=0)
+            welcomeMessage = tk.Label(self.master, text="Welcome "+self.username)
+            welcomeMessage.grid(row=0, column=0)
 
-            self.ok = tk.Button(self.master, text='ok', command=lambda: self.mainMenu())
-            self.ok.grid(row=1, column=0)
+            ok = tk.Button(self.master, text='ok', command=lambda: self.mainMenu())
+            ok.grid(row=1, column=0)
 
         else:
             self.clearFrame()
 
-            self.wrongMessage = tk.Label(self.master, text="Your username or password are incorrect.")
-            self.wrongMessage.grid(row=0, column=0)
+            wrongMessage = tk.Label(self.master, text="Your username or password are incorrect.")
+            wrongMessage.grid(row=0, column=0)
 
-            self.ok = tk.Button(self.master, text='ok', command=lambda: self.login())
-            self.ok.grid(row=1, column=0)
+            ok = tk.Button(self.master, text='ok', command=lambda: self.login())
+            ok.grid(row=1, column=0)
 
     def mainMenu(self):
         self.clearFrame()
 
-        self.quizzesLabel = tk.Label(self.master, text="Quizzes:")
-        self.quizzesLabel.grid(row=0, column=0, padx=10, pady=10)
+        quizzesLabel = tk.Label(self.master, text="Quizzes:")
+        quizzesLabel.grid(row=0, column=0, padx=10, pady=10)
 
-        self.additionBtn = tk.Button(self.master, text="addition", command=lambda: self.quizChoice("1"))
-        self.additionBtn.grid(row=0, column=1, padx=10, pady=10)
+        additionBtn = tk.Button(self.master, text="addition", command=lambda: self.quizChoice("1"))
+        additionBtn.grid(row=0, column=1, padx=10, pady=10)
 
-        self.subtractionBtn = tk.Button(self.master, text="subtraction", command=lambda: self.quizChoice("2"))
-        self.subtractionBtn.grid(row=0, column=2, padx=10, pady=10)
+        subtractionBtn = tk.Button(self.master, text="subtraction", command=lambda: self.quizChoice("2"))
+        subtractionBtn.grid(row=0, column=2, padx=10, pady=10)
 
-        self.multiplicationBtn = tk.Button(self.master, text="multiplication",
-                                           command=lambda: self.quizChoice("3"))
-        self.multiplicationBtn.grid(row=0, column=3, padx=10, pady=10)
+        multiplicationBtn = tk.Button(self.master, text="multiplication", command=lambda: self.quizChoice("3"))
+        multiplicationBtn.grid(row=0, column=3, padx=10, pady=10)
 
-        self.divisionBtn = tk.Button(self.master, text="division", command=lambda: self.quizChoice("4"))
-        self.divisionBtn.grid(row=0, column=4, padx=10, pady=10)
+        divisionBtn = tk.Button(self.master, text="division", command=lambda: self.quizChoice("4"))
+        divisionBtn.grid(row=0, column=4, padx=10, pady=10)
 
-        self.toolsLabel = tk.Label(self.master, text="Tools:")
-        self.toolsLabel.grid(row=1, column=0, padx=10, pady=10)
+        toolsLabel = tk.Label(self.master, text="Tools:")
+        toolsLabel.grid(row=1, column=0, padx=10, pady=10)
 
-        self.scoresBtn = tk.Button(self.master, text="previous Scores")
-        self.scoresBtn.grid(row=1, column=1, padx=10, pady=10)
+        scoresBtn = tk.Button(self.master, text="previous Scores")
+        scoresBtn.grid(row=1, column=1, padx=10, pady=10)
 
-        self.logoutBtn = tk.Button(self.master, text="logout", command=lambda: self.startMenu())
-        self.logoutBtn.grid(row=1, column=2, padx=10, pady=10)
+        logoutBtn = tk.Button(self.master, text="logout", command=lambda: self.startMenu())
+        logoutBtn.grid(row=1, column=2, padx=10, pady=10)
 
-        self.switchBtn = tk.Button(self.master, text="switch account", command=lambda: self.login())
-        self.switchBtn.grid(row=1, column=3, padx=10, pady=10)
+        switchBtn = tk.Button(self.master, text="switch account", command=lambda: self.login())
+        switchBtn.grid(row=1, column=3, padx=10, pady=10)
 
-        self.quitBtn = tk.Button(self.master, text="quit", command=self.master.destroy)
-        self.quitBtn.grid(row=1, column=4, padx=10, pady=10)
+        quitBtn = tk.Button(self.master, text="quit", command=self.master.destroy)
+        quitBtn.grid(row=1, column=4, padx=10, pady=10)
 
     def quizChoice(self, quiz):
         self.clearFrame()
@@ -222,24 +222,36 @@ class Gui(tk.Frame):
         questions = cursor.fetchall()
         numQu = 0
 
+        ans = tk.IntVar()
+
         for question in questions:
+            nextLevel = tk.IntVar(0)
+            while nextLevel != 1:
+                print("hello")
+                time.sleep(1)
 
-            self.questionLabel = tk.Label(self.master, text=question[2])
-            self.questionLabel.grid(row=0, column=0)
+                self.questionLabel = tk.Label(self.master, text=question[2])
+                self.questionLabel.grid(row=0, column=0)
 
-            self.ansBtn1 = tk.Button(self.master, text=question[3], command=(lambda: self.choice=question[3]))
-            self.ansBtn1.grid(row=1, column=0)
+                self.ansBtn1 = tk.Button(self.master, text=question[3], command=lambda: ans.set(3))
+                self.ansBtn1.grid(row=1, column=0)
 
-            self.ansBtn2 = tk.Button(self.master, text=question[4], command=(lambda: self.choice=question[4]))
-            self.ansBtn2.grid(row=1, column=1)
+                self.ansBtn2 = tk.Button(self.master, text=question[4], command=lambda: ans.set(4))
+                self.ansBtn2.grid(row=1, column=1)
 
-            self.ansBtn3 = tk.Button(self.master, text=question[5], command=(lambda: self.choice=question[5]))
-            self.ansBtn3.grid(row=2, column=0)
+                self.ansBtn3 = tk.Button(self.master, text=question[5], command=lambda: ans.set(5))
+                self.ansBtn3.grid(row=2, column=0)
 
-            self.ansBtn4 = tk.Button(self.master, text=question[6], command=(lambda: self.choice=question[6]))
-            self.ansBtn4.grid(row=2, column=1)
+                self.ansBtn4 = tk.Button(self.master, text=question[6], command=lambda: ans.set(6))
+                self.ansBtn4.grid(row=2, column=1)
 
-            if self.choice == question[7]:
+                self.ok = tk.Button(self.master, text="Submit answer", command=lambda: nextlevel.set(1))
+                self.ok.grid(row=2, column=1)
+                print("hi")
+                time.sleep(1)
+
+
+            if question[x] == question[7]:
                 self.clearFrame()
 
                 self.congrats = tk.Label(self.master, text="Correct")
